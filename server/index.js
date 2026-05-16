baf80dacc86d507cf5768205384c0967aacaf995
Rayyanmomin05 <rayyanmomin2005@gmail.com>
2026-05-16 14:53:22 +0530

    Remove dotenv for Render compatibility

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 
 const aiRoute = require('./routes/ai');
 
@@ -9,7 +8,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
 app.use('/api', aiRoute);
 
 app.get('/', (req, res) => {
